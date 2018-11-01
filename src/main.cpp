6d1e2ad9d8e3b141219ad53a470d7a3fc03f33e9
Claudio Matsuoka <cmatsuoka@gmail.com>
2018-11-01 11:46:09 -0300

    Parse font name and directory from command line
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,82 +5,27 @@
 #include "cxxopts.hpp"
 
 void print_output(FIGline);
-static std::wstring to_wstring(const char *);
+
+constexpr int DefaultWidth = 80;
+constexpr char DefaultFont[] = "standard.flf";
+constexpr char DefaultDir[] = "/usr/share/figlet";
 
 
-int main(int argc, char *argv[])
+namespace {
+
+std::string find_font(std::string const& dir, std::string const& name)
 {
-    std::setlocale(LC_CTYPE, "");
+    std::string path = dir + File::separator() + name;
 
-    cxxopts::Options options(argv[0]);
-    options.add_options()
-        ("c,center", "center the output horizontally")
-        ("h,help", "show usage information and exit")
-        ("k,kern", "use kerning mode to display characters")
-        ("l,left", "left-align the output")
-        ("o,overlap", "use character overlapping mode")
-        ("p,paragraph", "ignore mid-paragraph line breaks")
-        ("r,right", "right-align the output")
-        ("S,smush", "use smushing mode to display characters")
-        ("W,full-width", "display characters in full width");
-
-    try {
-        auto opt = options.parse(argc, argv);
-
-        if (opt.count("help")) {
-            std::cout << options.help() << std::endl;
-            exit(EXIT_SUCCESS);
-        }
-    
-        std::wstring msg;
-        if (argc > 0) {
-            msg += to_wstring(argv[1]);
-            for (int i = 2; i < argc; i++) {
-                msg = msg + L" " + to_wstring(argv[i]);
-            }
-        }
-    
-        auto font = FIGfont("fonts/standard.flf");
-        auto sm = Smusher(font);
-    
-        if (opt.count("o")) {
-            sm.mode = 0;
-        } else if (opt.count("k")) {
-            sm.mode = SmushKern;
-        } else if (opt.count("W")) {
-            sm.full_width = true;
-        }
-    
-        auto wr = Wrapper(sm, 80);
-    
-        if (opt.count("center")) {
-           wr.align = Align::Center;
-        } else if (opt.count("right")) {
-           wr.align = Align::Right;
-        } else if (opt.count("left")) {
-           wr.align = Align::Left;
-        }
-    
-        wr.clear();
-        wr.wrap_str(msg, print_output);
-        print_output(wr.get());
-    } catch (std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        exit(EXIT_FAILURE);
-    }
-}
-
-void print_output(FIGline v)
-{
-    std::wcout << v << std::flush;
+    // FIXME: add extension, check if file exists
+    return path;
 }
 
 
 // From https://stackoverflow.com/questions/1664476/is-it-possible-to-use-a-unicode-argv
-
 #define ARR_LEN(x) (sizeof(x)/sizeof(x[0]))
 
-static std::wstring to_wstring(const char *s)
+std::wstring to_wstring(const char *s)
 {
     typedef unsigned char byte;
     struct Level {
@@ -138,5 +83,83 @@ static std::wstring to_wstring(const char *s)
         }
     }
     return result;
-}   // wstring Convert
+}
+
+}  // namespace
+
+
+int main(int argc, char *argv[])
+{
+    std::setlocale(LC_CTYPE, "");
+
+    int cols = DefaultWidth;
+    std::string font_dir(DefaultDir);
+    std::string font_name(DefaultFont);
+
+    cxxopts::Options options(argv[0]);
+    options.add_options()
+        ("c,center", "center the output horizontally")
+        ("d,dir", "set the default font directory", cxxopts::value<std::string>(font_dir))
+        ("f,font", "specify the figfont to use", cxxopts::value<std::string>(font_name))
+        ("h,help", "show usage information and exit")
+        ("k,kern", "use kerning mode to display characters")
+        ("l,left", "left-align the output")
+        //("m,mode", "override the font layout mode", cxxopts::value<uint32_t>(num));
+        ("o,overlap", "use character overlapping mode")
+        ("p,paragraph", "ignore mid-paragraph line breaks")
+        ("r,right", "right-align the output")
+        ("S,smush", "use smushing mode to display characters")
+        ("W,full-width", "display characters in full width")
+        ("w,width", "set the output width", cxxopts::value<int>(cols));
+
+    try {
+        auto opt = options.parse(argc, argv);
+
+        if (opt.count("help")) {
+            std::cout << options.help() << std::endl;
+            exit(EXIT_SUCCESS);
+        }
+    
+        std::wstring msg;
+        if (argc > 0) {
+            msg += to_wstring(argv[1]);
+            for (int i = 2; i < argc; i++) {
+                msg = msg + L" " + to_wstring(argv[i]);
+            }
+        }
+
+        auto font = FIGfont(find_font(font_dir, font_name));
+        auto sm = Smusher(font);
+    
+        if (opt.count("o")) {
+            sm.mode = 0;
+        } else if (opt.count("k")) {
+            sm.mode = SmushKern;
+        } else if (opt.count("W")) {
+            sm.full_width = true;
+        }
+    
+        auto wr = Wrapper(sm, 80);
+    
+        if (opt.count("center")) {
+           wr.align = Align::Center;
+        } else if (opt.count("right")) {
+           wr.align = Align::Right;
+        } else if (opt.count("left")) {
+           wr.align = Align::Left;
+        }
+    
+        wr.clear();
+        wr.wrap_str(msg, print_output);
+        print_output(wr.get());
+    } catch (std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+void print_output(FIGline v)
+{
+    std::wcout << v << std::flush;
+}
 
